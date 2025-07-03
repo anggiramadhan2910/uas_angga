ed121cb5d24aafb60c66f0fb633fd4d19d74a1c6
anggiramadhan2910 <anggiramadhan2910@gmail.com>
2025-07-03 11:24:43 +0700

    route custom table

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\CustomController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+Route::resource('customs', CustomController::class);
 });
